5e6508942f1a351498b69c0c46aec17b2e20fb29
bhadreshdudhat <bhadreshdudhat@gmail.com>
2019-05-15 19:24:09 -0500

    add navigation routes for videos and notes components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,8 @@ import { UsersComponent } from './users/users.component';
 import { TenantsComponent } from './tenants/tenants.component';
 import { RolesComponent } from 'app/roles/roles.component';
 import { ChangePasswordComponent } from './users/change-password/change-password.component';
+import { MyVideosComponent } from './my-videos/my-videos.component';
+import { MyNotesComponent } from './my-notes/my-notes.component';
 
 @NgModule({
     imports: [
@@ -21,7 +23,9 @@ import { ChangePasswordComponent } from './users/change-password/change-password
                     { path: 'roles', component: RolesComponent, data: { permission: 'Pages.Roles' }, canActivate: [AppRouteGuard] },
                     { path: 'tenants', component: TenantsComponent, data: { permission: 'Pages.Tenants' }, canActivate: [AppRouteGuard] },
                     { path: 'about', component: AboutComponent },
-                    { path: 'update-password', component: ChangePasswordComponent }
+                    { path: 'update-password', component: ChangePasswordComponent },
+                    { path: 'myvideos', component: MyVideosComponent, data: { permission: 'Pages.Users' }, canActivate: [AppRouteGuard] },
+                    { path: 'mynotes', component: MyNotesComponent, data: { permission: 'Pages.Users' }, canActivate: [AppRouteGuard] }
                 ]
             }
         ])
