636318c5f8fc54f9b255e8c58fb1ad3b8cbf0b2b
bhadreshdudhat <bhadreshdudhat@gmail.com>
2019-05-15 19:17:28 -0500

    generate my-notes component

---
+++ src/app/my-notes/my-notes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-notes',
+  templateUrl: './my-notes.component.html',
+  styleUrls: ['./my-notes.component.css']
+})
+export class MyNotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
