e03a05f4a261d1122830d73a84996097fdf49620
bhadreshdudhat <bhadreshdudhat@gmail.com>
2019-05-15 23:37:04 -0500

    get loggedin user in notes component by extending AppComponentBase

--- src/app/my-notes/my-notes.component.ts
+++ src/app/my-notes/my-notes.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, Injector, ViewEncapsulation } from '@angular/core';
+import { AppComponentBase } from '@shared/app-component-base';
+import { AppAuthService } from '@shared/auth/app-auth.service';
 
 @Component({
   selector: 'my-notes',
   templateUrl: './my-notes.component.html',
   styleUrls: ['./my-notes.component.css']
 })
-export class MyNotesComponent implements OnInit {
+export class MyNotesComponent extends AppComponentBase implements OnInit{
 
-  constructor() { }
+  loginUser = '';
 
-  ngOnInit() {
-  }
+    constructor( injector: Injector) {
+        super(injector);
+    }
+
+    ngOnInit() {
+        this.loginUser = this.appSession.user.emailAddress;
+    }
 
 }
