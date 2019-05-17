a17a8eb855b31e059472d5418361be1df4157f2f
bhadreshdudhat <bhadreshdudhat@gmail.com>
2019-05-17 03:01:01 -0500

    Create NotesService for CRUD on notes

---
+++ src/app/my-notes/notes.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserNote } from './UserNote';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotesService {
+
+  constructor(private http:HttpClient) { }
+
+  createNote(userNote:UserNote){
+    var body = JSON.stringify(userNote);
+    console.log(body);
+    let headers={
+      headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+      })
+  }
+	return this.http.post('http://localhost:21021/api/services/app/NotesService/CreateNote',body, headers);
+   
+  }
+}
