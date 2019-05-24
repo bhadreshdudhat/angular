3eddb84afbb1500115e4856553d5dca7f3d7877f
bhadreshdudhat <bhadreshdudhat@gmail.com>
2019-05-24 14:13:05 -0500

    Add child paths for 4 graphs in saleschart routing file

--- src/app/sales-chart/sales-chart-routing.module.ts
+++ src/app/sales-chart/sales-chart-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SalesChartComponent } from './sales-chart.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
+import { RadarChartComponent } from './radar-chart/radar-chart.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', // it's actually app/saleschart So routerLink="/app/saleschart/bar-chart" in view
+    component: SalesChartComponent,
+    children: 
+    [
+      { path: '', redirectTo: '/app/saleschart/bar-chart', pathMatch: 'full' },
+      { path: 'bar-chart', component: BarChartComponent },
+      { path: 'doughnut-chart', component: DoughnutChartComponent },
+      { path: 'radar-chart', component: RadarChartComponent }, 
+      { path: 'pie-chart', component: PieChartComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
