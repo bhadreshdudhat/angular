4f8778b610911ae4957bd98a14e08cb9ba976a01
bhadreshdudhat <bhadreshdudhat@gmail.com>
2019-05-24 14:07:41 -0500

    Add 4 different components with chart functionality using static data which represents 4 different graphs

---
+++ src/app/sales-chart/pie-chart/pie-chart.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pie-chart',
+  templateUrl: './pie-chart.component.html',
+  styleUrls: ['./pie-chart.component.css']
+})
+export class PieChartComponent implements OnInit {
+
+    
+  public pieChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
+  public pieChartData = [120, 150, 180, 90];
+  public pieChartType = 'pie';
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
