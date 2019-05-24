b79d8f52c1fae0e718850ea9818dbefd862c7e03
bhadreshdudhat <bhadreshdudhat@gmail.com>
2019-05-24 13:57:04 -0500

    Add SalesChart Component to SalesChart Module

--- src/app/sales-chart/sales-chart.module.ts
+++ src/app/sales-chart/sales-chart.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SalesChartRoutingModule } from './sales-chart-routing.module';
+import { SalesChartComponent } from './sales-chart.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SalesChartComponent],
   imports: [
     CommonModule,
     SalesChartRoutingModule
