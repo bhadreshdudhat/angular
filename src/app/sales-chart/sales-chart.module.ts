e79ae25f44614af6bca44e987e3854f3da9287cf
bhadreshdudhat <bhadreshdudhat@gmail.com>
2019-05-24 14:08:27 -0500

    add 4 graph components to saleschart module

--- src/app/sales-chart/sales-chart.module.ts
+++ src/app/sales-chart/sales-chart.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { SalesChartRoutingModule } from './sales-chart-routing.module';
 import { SalesChartComponent } from './sales-chart.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { RadarChartComponent } from './radar-chart/radar-chart.component';
+import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
 
 @NgModule({
-  declarations: [SalesChartComponent],
+  declarations: [SalesChartComponent, BarChartComponent, PieChartComponent, RadarChartComponent, DoughnutChartComponent],
   imports: [
     CommonModule,
     SalesChartRoutingModule
