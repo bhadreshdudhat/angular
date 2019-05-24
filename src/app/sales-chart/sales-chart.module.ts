8f33af87b2cac66e760f26cb1943c941cf1a0e6f
bhadreshdudhat <bhadreshdudhat@gmail.com>
2019-05-24 13:47:04 -0500

    add new module with routing  in app directory

---
+++ src/app/sales-chart/sales-chart.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SalesChartRoutingModule } from './sales-chart-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SalesChartRoutingModule
+  ]
+})
+export class SalesChartModule { }
