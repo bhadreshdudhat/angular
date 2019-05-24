468cd86edfa61c5a18e208f16c6cab1e67943914
bhadreshdudhat <bhadreshdudhat@gmail.com>
2019-05-24 13:55:24 -0500

    add component for saleschart

---
+++ src/app/sales-chart/sales-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sales-chart',
+  templateUrl: './sales-chart.component.html',
+  styleUrls: ['./sales-chart.component.css']
+})
+export class SalesChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
